7df889c22ee9e61ffbf50a7cf07b79a72f2342e4
unknown <reynaldogm95@gmail.com>
2018-07-02 22:43:10 -0500

    Primer commit

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,24 @@
+import{Component} from '@angular/core';
+
+
+@Component({
+selector: 'app-body',
+templateUrl:'./body.component.html'
+
+})
+export class BodyComponent{
+
+
+
+  personajes: string[] =['Spiderman','Dr.Doom','Venom'];
+    mostrar = true;
+
+  frase: any {
+
+      mensaje:"Un gran poder conlleva una gran responsabilidad",
+      autor: "Reynaldo"
+
+
+  }
+
+}
